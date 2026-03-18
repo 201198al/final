cf105513c099c601e7ffb85fdd6aff01ca5fe412
201198al <201198al@gmail.com>
2026-03-18 22:29:31 +1000

    Стили

--- scan-project/src/App.jsx
+++ scan-project/src/App.jsx
@@ -1,10 +1,20 @@
-import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Search from "./pages/Search.jsx";
+import Result from "./pages/Result.jsx";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- scan-project/src/pages/Home.jsx
+++ scan-project/src/pages/Home.jsx
@@ -1,5 +1,49 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h2>Главная страница</h2>;
+  return (
+    <div>
+     
+      <header>
+        <h1>SCAN</h1>
+        <nav>
+          <Link to="/login">Войти</Link>
+        </nav>
+      </header>
+
+      
+      <section>
+        <h2>Сервис по поиску публикаций</h2>
+        <p>Анализируем новости и данные</p>
+        <button>Начать поиск</button>
+      </section>
+
+     
+     <section>
+  <h2>Наши тарифы</h2>
+
+  <div className="tariffs">
+    <div className="card">
+      <h3>Beginner</h3>
+      <p>Для старта</p>
+      <button>Выбрать</button>
+    </div>
+
+    <div className="card">
+      <h3>Pro</h3>
+      <p>Для работы</p>
+      <button>Выбрать</button>
+    </div>
+
+    <div className="card">
+      <h3>Business</h3>
+      <p>Для компаний</p>
+      <button>Выбрать</button>
+    </div>
+  </div>
+</section>
+    </div>
+  );
 }
 
 export default Home;
