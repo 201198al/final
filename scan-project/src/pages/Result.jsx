5fcad7524cf3a9d25a1a2db82d5a7534214fdf6b
201198al <201198al@gmail.com>
2026-03-19 20:46:59 +1000

    Результаты

--- scan-project/src/pages/Result.jsx
+++ scan-project/src/pages/Result.jsx
@@ -1,5 +1,17 @@
 function Result() {
-  return <h2>Результаты</h2>;
+  return (
+    <div>
+      <h2>Результаты</h2>
+
+      <div className="card">
+        <p>Новость 1: Компания растет</p>
+      </div>
+
+      <div className="card">
+        <p>Новость 2: Новый контракт</p>
+      </div>
+    </div>
+  );
 }
 
 export default Result;
--- scan-project/src/pages/Search.jsx
+++ scan-project/src/pages/Search.jsx
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 function Search() {
-  return <h2>Поиск</h2>;
+  const [inn, setInn] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    navigate("/result");
+  };
+
+  return (
+    <div>
+      <h2>Поиск компании</h2>
+
+      <input
+        type="text"
+        placeholder="Введите ИНН"
+        value={inn}
+        onChange={(e) => setInn(e.target.value)}
+      />
+
+      <br />
+
+      <button onClick={handleSearch}>Искать</button>
+    </div>
+  );
 }
 
 export default Search;
