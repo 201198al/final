acdb24b810995f11665fdad3d1a819a819b4f0ce
201198al <201198al@gmail.com>
2026-03-21 18:09:31 +1000

    Карточки новостей

--- scan-project/src/pages/Result.jsx
+++ scan-project/src/pages/Result.jsx
@@ -4,16 +4,26 @@ function Result() {
   const location = useLocation();
   const inn = location.state?.inn;
 
+  // ФЕЙКОВЫЕ ДАННЫЕ (как будто с API)
+  const news = [
+    { id: 1, title: "Компания растет", text: "Доход увеличился на 20%" },
+    { id: 2, title: "Новая сделка", text: "Подписан крупный контракт" },
+    { id: 3, title: "Расширение", text: "Открытие нового офиса" },
+  ];
+
   return (
     <div>
       <h2>Результаты поиска</h2>
 
       <p>Вы искали ИНН: {inn}</p>
 
-      <div>
-        <p>Новость 1 про компанию</p>
-        <p>Новость 2 про компанию</p>
-        <p>Новость 3 про компанию</p>
+      <div className="news">
+        {news.map((item) => (
+          <div key={item.id} className="news-card">
+            <h3>{item.title}</h3>
+            <p>{item.text}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
