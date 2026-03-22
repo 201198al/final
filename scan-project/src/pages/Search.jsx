c864dacda205f33dc7606ce6b45e7550d7e80600
201198al <201198al@gmail.com>
2026-03-22 14:13:07 +1000

    Валидация ИНН

--- scan-project/src/pages/Search.jsx
+++ scan-project/src/pages/Search.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function Search() {
   const [inn, setInn] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
  
@@ -14,6 +15,12 @@ function Search() {
   }, []);
 
  const handleSearch = () => {
+  if (inn.length !== 10 && inn.length !== 12) {
+    setError("ИНН должен быть 10 или 12 цифр");
+    return;
+  }
+
+  setError("");
   localStorage.setItem("inn", inn);
   navigate("/result");
 };
@@ -22,12 +29,15 @@ function Search() {
     <div className="container">
       <h2>Поиск компании</h2>
 
-      <input
-        type="text"
-        placeholder="Введите ИНН"
-        value={inn}
-        onChange={(e) => setInn(e.target.value)}
-      />
+    <input
+  type="text"
+  placeholder="Введите ИНН"
+  value={inn}
+  onChange={(e) => {
+    setInn(e.target.value);
+    setError("");
+  }}
+/>{error && <p style={{ color: "red" }}>{error}</p>} 
 
       <button onClick={handleSearch}>Поиск</button>
     </div>
